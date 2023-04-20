85aefe1b537d8abcd9e222d02bef17fb76bb47d2
Abraham Ruiz Carmona <alum.aruizc.2020@iesalixar.org>
2023-04-20 23:23:20 +0200

    Conf login-backend ver 1.0

---
+++ Backend/config/properties.js
@@ -0,0 +1,4 @@
+module.exports = {
+    PORT: process.env.PORT || 5000,
+    DB: 'mongodb+srv://tractor:tractor@cluster0.sygmdck.mongodb.net/proyecto-espatractor?retryWrites=true&w=majority'
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,14 @@
-console.log("SE HA ENCENDIDO EL SERVIDOR");
+'use strict'
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const propierties = require('./config/properties');
+const DB = require('./config/db');
+// iniciamos la base de datos
+DB();
+
+app.use(express.json());
+
+app.use('/api', require('./routes/user.router'));
+
+app.listen(propierties.PORT, () => console.log(`Server runing on port ${propierties.PORT}`));
