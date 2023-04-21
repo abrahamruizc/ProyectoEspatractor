6accc52f3201c65cf06966c8fd989406ed4472a1
Abraham Ruiz Carmona <alum.aruizc.2020@iesalixar.org>
2023-04-21 16:30:14 +0200

    Mejora conf backend

--- Backend/config/properties.js
+++ Backend/config/properties.js
@@ -1,4 +1,5 @@
+require('dotenv').config();
 module.exports = {
-    PORT: process.env.PORT || 5000,
-    DB: 'mongodb+srv://tractor:tractor@cluster0.sygmdck.mongodb.net/proyecto-espatractor?retryWrites=true&w=majority'
+    PORT: process.env.PORT || 3000,
+    DB: process.env.DB_URI
 }
